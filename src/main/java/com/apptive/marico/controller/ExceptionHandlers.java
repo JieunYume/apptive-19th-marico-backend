763b383d60623d208235805db20698c5bba5d63b
이지은 <jieun7056@naver.com>
2023-11-16 00:38:52 +0900

    feat: member와 stylist의 로그인 기능에서 아이디 혹은 비밀번호가 틀렸을 때 예외 처리 구현

--- src/main/java/com/apptive/marico/controller/ExceptionHandlers.java
+++ src/main/java/com/apptive/marico/controller/ExceptionHandlers.java
@@ -5,10 +5,13 @@ import com.apptive.marico.dto.error.UnknownErrorDto;
 import com.apptive.marico.exception.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.apptive.marico.exception.ErrorCode.INTERNAL_SERVER_ERROR;
+import static com.apptive.marico.exception.ErrorCode.*;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
@@ -22,5 +25,12 @@ public class ExceptionHandlers {
         System.out.println(exception.getMessage());
         exception.printStackTrace();
         return new ResponseEntity(new UnknownErrorDto(INTERNAL_SERVER_ERROR.getStatus(), INTERNAL_SERVER_ERROR.getMessage(), exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({ BadCredentialsException.class })
+    protected ResponseEntity handleAuthenticationException(BadCredentialsException exception) {
+        System.out.println(exception.getMessage());
+        exception.printStackTrace();
+        return new ResponseEntity(new ErrorDto(ID_OR_PASSWORD_NOT_MATCH.getStatus(), ID_OR_PASSWORD_NOT_MATCH.getMessage()), HttpStatus.valueOf(ID_OR_PASSWORD_NOT_MATCH.getStatus()));
     }
 }
