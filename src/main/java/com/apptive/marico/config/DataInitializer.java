dac14df254ec7b2dd16cbade2fe4af1135b8e179
이지은 <jieun7056@naver.com>
2026-06-04 01:27:41 +0900

    feat: role 테이블에 3개 값이 자동으로 들어가도록 수정

---
+++ src/main/java/com/apptive/marico/config/DataInitializer.java
@@ -0,0 +1,23 @@
+package com.apptive.marico.config;
+
+import com.apptive.marico.entity.Role;
+import com.apptive.marico.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) { // 서버가 실행될 때 run
+        for (Role.RoleName roleName : Role.RoleName.values()) {
+            if (roleRepository.findByName(roleName).isEmpty()) {
+                roleRepository.save(new Role(roleName));
+            }
+        }
+    }
+}
